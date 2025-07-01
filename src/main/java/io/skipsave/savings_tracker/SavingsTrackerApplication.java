3e3c20f717bc5dafb7a7d83ce21395acfe7681fa
remziyecigci <remziyecigci@gmail.com>
2025-07-01 11:00:38 +0200

    Add updated application.properties

--- src/main/java/io/skipsave/savings_tracker/SavingsTrackerApplication.java
+++ src/main/java/io/skipsave/savings_tracker/SavingsTrackerApplication.java
@@ -1,4 +1,4 @@
-package io.skipsave.savings_tracker.starter;
+package io.skipsave.savings_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
