cf232dabd00668641cd9cfc3e246780875f67e40
remziyecigci <remziyecigci@gmail.com>
2025-07-04 10:43:00 +0200

    Add global exception handler for runtime exceptions

--- src/main/java/io/skipsave/savings_tracker/exception/GlobalExceptionHandler.java
+++ src/main/java/io/skipsave/savings_tracker/exception/GlobalExceptionHandler.java
@@ -17,4 +17,10 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeExceptions(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 }
